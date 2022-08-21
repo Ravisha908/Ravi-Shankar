c01a072f449c80885ffd18a487e05fa51785d67f
unknown <ravisankar908@gmail.com>
2022-08-21 13:34:32 +0530

    Updates

---
+++ precache-manifest.d111617582cf9aa756526684f9ef63cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad78d765880f77362fc",
+    "url": "/Ravisha908/Ravi-Shankar.git/static/css/main.cbdf5580.chunk.css"
+  },
+  {
+    "revision": "0ad78d765880f77362fc",
+    "url": "/Ravisha908/Ravi-Shankar.git/static/js/main.61b19d7b.chunk.js"
+  },
+  {
+    "revision": "9458348498f0d0f60695",
+    "url": "/Ravisha908/Ravi-Shankar.git/static/js/runtime~main.a3c4f731.js"
+  },
+  {
+    "revision": "158034e95928bbb4f750",
+    "url": "/Ravisha908/Ravi-Shankar.git/static/js/2.4681e3f3.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Ravisha908/Ravi-Shankar.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2ee3e1f7a29c0525bd290ee4184aa71e",
+    "url": "/Ravisha908/Ravi-Shankar.git/index.html"
+  }
+];
